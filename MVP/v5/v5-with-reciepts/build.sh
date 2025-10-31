b8d9dd432060dc4259bd849d7c21babf1f4c425e
OBINexus TDA Agent <tda@obinexus.org>
2025-10-31 11:14:50 +0000

    W

--- MVP/v5/v5-with-reciepts/build.sh
+++ MVP/v5/v5-with-reciepts/build.sh
@@ -1,2 +1,2 @@
-gcc -std=gnu11 -O2 -Wall -Wextra -pthread -fsanitize=address,undefined -g ...  -o bin/diramc-with-reciept-v2of5. ./src/main.c
+gcc -std=gnu11 -O2 -Wall -Wextra -pthread -fsanitize=address,undefined -g ...  -o bin/diram-with-reciepts ./src/main.c
 
